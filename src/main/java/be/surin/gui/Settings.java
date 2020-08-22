b6a928705653163781ca651860fb422d032d75b1
Virgil Surin <virgil.surin@student.umons.ac.be>
2020-08-22 19:24:43 +0200

    Course collector

--- src/main/java/be/surin/gui/Settings.java
+++ src/main/java/be/surin/gui/Settings.java
@@ -1,10 +1,13 @@
 package be.surin.gui;
 
+import javafx.collections.FXCollections;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
 
 public class Settings {
 
@@ -14,6 +17,7 @@ public class Settings {
     public static Scene settingsSetup() {
         BorderPane mainPane = new BorderPane();
         settingsPane = new TabPane();
+        settingsPane.setTabClosingPolicy(TabPane.TabClosingPolicy.UNAVAILABLE);
 
         Tab hppTab = new Tab();
         hppTab.setText("HyperPlanning");
@@ -48,8 +52,45 @@ public class Settings {
         courseSelect.getColumns().addAll(optionColumn, courseColumn, codeColumn);
 
         courseSelect.setPlaceholder(new Label("Aucun cours disponible"));
+
+        //add a course
+        Button addCourse = new Button("Ajouter un cours");
+        addCourse.setOnAction(e -> {
+            addStage();
+        });
+
         bp.setCenter(courseSelect);
         return bp;
+    }
+
+    public static void addStage() {
+        Stage addStage = new Stage();
+        VBox layout = new VBox();
+
+        Label optionPick = new Label("Choisissez une option");
+        ChoiceBox<String> optionChoice = new ChoiceBox<>();
+        optionChoice.setItems(FXCollections.observableArrayList(
+                "BAC1-Math",
+                "BAC2-Math",
+                "BAC3-Math",
+                "BAC1-Info",
+                "BAC2-Info",
+                "BAC3-Info",
+                "BAC1-Physique",
+                "BAC2-Physique",
+                "BAC3-Physique",
+                "BAC1-Biologie",
+                "BAC2-Biologie",
+                "BAC3-Biologie",
+                "BAC1-Chimie",
+                "BAC2-Chimie",
+                "BAC3-Chimie" ));
+        
+
+
+        Scene sc = new Scene(layout);
+        addStage.setScene(sc);
+        addStage.show();
     }
 
     public static TabPane getSettingsTabs() {
--- src/main/java/be/surin/web/HyperPlanning2020Collector.java
+++ src/main/java/be/surin/web/HyperPlanning2020Collector.java
@@ -1,11 +1,104 @@
 package be.surin.web;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.List;
+
 public class HyperPlanning2020Collector {
 
     static final String url = "https://hplanning2020.umons.ac.be/invite";
     static final String geckoDriverPath = "src\\main\\resources\\geckodriver.exe";
 
     public static void main(String[] args) {
+        getCourse();
+    }
+
+    public static void getCourse() {
+        String BAB1MATH = "//*[@id=\"GInterface.Instances[1].Instances[1]_15\"]";
+        String BAB2MATH = "//*[@id=\"GInterface.Instances[1].Instances[1]_39\"]";
+        String BAB3MATH = "//*[@id=\"GInterface.Instances[1].Instances[1]_62\"]";
+
+        String BAB1INFO = "//*[@id=\"GInterface.Instances[1].Instances[1]_14\"]";
+        String BAB2INFO = "//*[@id=\"GInterface.Instances[1].Instances[1]_38\"]";
+        String BAB3INFO = "//*[@id=\"GInterface.Instances[1].Instances[1]_61\"]";
+
+        String[] optionArray = {
+                //math
+                "//*[@id=\"GInterface.Instances[1].Instances[1]_15\"]",
+                "//*[@id=\"GInterface.Instances[1].Instances[1]_39\"]",
+                "//*[@id=\"GInterface.Instances[1].Instances[1]_62\"]",
+                //info
+                "//*[@id=\"GInterface.Instances[1].Instances[1]_14\"]",
+                "//*[@id=\"GInterface.Instances[1].Instances[1]_38\"]",
+                "//*[@id=\"GInterface.Instances[1].Instances[1]_61\"]"
+        };
+
+        /*
+        BAC1-Physique
+-
+BAC2-Physique
+-
+BAC3-Physique
+-
+BAC1-Biologie
+-
+BAC2-Biologie
+-
+BAC3-Biologie
+-
+BAC1-Chimie
+-
+BAC2-Chimie
+-
+BAC3-Chimie
+         */
+        String buttonEdit = "//*[@id=\"GInterface.Instances[1].Instances[1].bouton_Edit\"]";
+        String scrollBar = "//*[@id=\"GInterface.Instances[1].Instances[1]_ContenuScroll\"]";
+        String weekBar = "//*[@id=\"GInterface.Instances[1].Instances[4]_Calendrier\"]";
+        String recap = "/html/body/div[3]/div[1]/table/tbody/tr[1]/td/div[3]/div[1]/ul/li[2]";
+
+        String table = "/html/body/div[3]/div/table/tbody/tr[2]/td/table/tbody/tr[2]/td/div/div[2]/div/div[1]/table[1]/tbody";
+        String table2 = "/html/body/div[3]/div[1]/table/tbody/tr[2]/td/table/tbody/tr[2]/td/div/div[2]/div/div[1]/table[1]";
+
+        System.setProperty("webdriver.gecko.driver", geckoDriverPath);
+
+        FirefoxOptions fxOption = new FirefoxOptions();
+        fxOption.addArguments("--headless", "--disable-gpu", "--window-size=1920,1200","--ignore-certificate-errors");
+        FirefoxDriver driver = new FirefoxDriver(fxOption);
+        WebDriverWait wait = new WebDriverWait(driver, 100);
+        driver.navigate().to(url);
+
+        //TODO only for BAB2 math for now, change i=1 to i=0 and uncomment optionArray.length for full loop over BaB1 to 3 math and info
+        for(int i=1; i<2/*optionArray.length*/; i++) {
+
+            // Click the scrollbar (after it's loaded)
+            wait.until(ExpectedConditions.elementToBeClickable(By.xpath(buttonEdit)));
+            driver.findElement(By.xpath(buttonEdit)).click();
+
+            // Find the option button and click it
+            wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(scrollBar)));
+            driver.findElement(By.xpath(optionArray[i])).click();
+
+            // Click to go to "récapitulatif des cours"
+            wait.until(ExpectedConditions.visibilityOfAllElementsLocatedBy(By.xpath(recap)));
+            driver.findElement(By.xpath(recap)).click();
+            List<WebElement> rows = driver.findElements(By.xpath(table2));
+
+
+            for (WebElement row : rows) {
+                System.out.println(row.getText());
+            }
+
+            driver.quit();
+        }
+
+
+
 
     }
 }
