e5894d4eb397e2ba9257c1c00b4c85a6f12c752f
Virgil Surin <virgil.surin@student.umons.ac.be>
2020-08-23 00:44:07 +0200

    created text file with course for BAB1 to BAB3 math + info

--- src/main/java/be/surin/web/HyperPlanning2020Collector.java
+++ src/main/java/be/surin/web/HyperPlanning2020Collector.java
@@ -1,24 +1,35 @@
 package be.surin.web;
 
+import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.phantomjs.PhantomJSDriver;
+import org.openqa.selenium.phantomjs.PhantomJSDriverService;
+import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.io.*;
 import java.util.List;
+
+import static be.surin.web.HyperPlanning2020Scraper.phantomJSPath;
 
 public class HyperPlanning2020Collector {
 
     static final String url = "https://hplanning2020.umons.ac.be/invite";
     static final String geckoDriverPath = "src\\main\\resources\\geckodriver.exe";
+    static final String chromeDriverPath = "src\\main\\resources\\chromedriver.exe";
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         getCourse();
     }
 
-    public static void getCourse() {
+    public static void getCourse() throws IOException {
         String BAB1MATH = "//*[@id=\"GInterface.Instances[1].Instances[1]_15\"]";
         String BAB2MATH = "//*[@id=\"GInterface.Instances[1].Instances[1]_39\"]";
         String BAB3MATH = "//*[@id=\"GInterface.Instances[1].Instances[1]_62\"]";
@@ -62,19 +73,21 @@ BAC3-Chimie
         String weekBar = "//*[@id=\"GInterface.Instances[1].Instances[4]_Calendrier\"]";
         String recap = "/html/body/div[3]/div[1]/table/tbody/tr[1]/td/div[3]/div[1]/ul/li[2]";
 
-        String table = "/html/body/div[3]/div/table/tbody/tr[2]/td/table/tbody/tr[2]/td/div/div[2]/div/div[1]/table[1]/tbody";
-        String table2 = "/html/body/div[3]/div[1]/table/tbody/tr[2]/td/table/tbody/tr[2]/td/div/div[2]/div/div[1]/table[1]";
+        String table = "/html/body/div[3]/div/table/tbody/tr[2]/td/table/tbody/tr[2]/td/div/div[2]/div/div[1]/table[1]/tbody"; //tbody
+        String table2 = "/html/body/div[3]/div[1]/table/tbody/tr[2]/td/table/tbody/tr[2]/td/div/div[2]/div/div[1]/table[1]"; //table class
+        String table3 = "/html/body/div[3]/div[1]/table/tbody/tr[2]/td/table/tbody/tr[2]/td/div/div[2]/div/div[1]/table[1]/tbody/tr[2]"; //cell
 
         System.setProperty("webdriver.gecko.driver", geckoDriverPath);
 
         FirefoxOptions fxOption = new FirefoxOptions();
         fxOption.addArguments("--headless", "--disable-gpu", "--window-size=1920,1200","--ignore-certificate-errors");
         FirefoxDriver driver = new FirefoxDriver(fxOption);
+
         WebDriverWait wait = new WebDriverWait(driver, 100);
         driver.navigate().to(url);
 
         //TODO only for BAB2 math for now, change i=1 to i=0 and uncomment optionArray.length for full loop over BaB1 to 3 math and info
-        for(int i=1; i<2/*optionArray.length*/; i++) {
+        for(int i=0; i<optionArray.length; i++) {
 
             // Click the scrollbar (after it's loaded)
             wait.until(ExpectedConditions.elementToBeClickable(By.xpath(buttonEdit)));
@@ -87,15 +100,11 @@ BAC3-Chimie
             // Click to go to "récapitulatif des cours"
             wait.until(ExpectedConditions.visibilityOfAllElementsLocatedBy(By.xpath(recap)));
             driver.findElement(By.xpath(recap)).click();
-            List<WebElement> rows = driver.findElements(By.xpath(table2));
+            List<WebElement> rows = driver.findElements(By.xpath(table));
+            FileUtils.writeStringToFile(new File("src\\main\\resources\\course"+i+".txt"), rows.get(0).getText());
 
-
-            for (WebElement row : rows) {
-                System.out.println(row.getText());
-            }
-
-            driver.quit();
         }
+        driver.quit();
 
 
 
